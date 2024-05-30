23adcc1b8a42b1a091695084335ae51c11a624a5
sxz-isahxx <suyane.carvalho@escola.pr.gov.br>
2024-05-30 14:57:06 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+    background("lightgrey")
+  
+    textFont("Comic Sans MS");
+    textSize(30)
+    textAlign(CENTER,CENTER);
+  
+  }
+  
+  function draw(){
+    text("Professor Malcolm divo s2",200,200)
+  }
+  
